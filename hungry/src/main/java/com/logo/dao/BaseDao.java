13b47d92929531f1e66d77dee3c20e4ba576af33
Administrator <Administrator@DESKTOP-E07254S.lan>
2017-11-15 10:32:23 +0800

    第一次项目提交，项目框架

---
+++ hungry/src/main/java/com/logo/dao/BaseDao.java
@@ -0,0 +1,16 @@
+package com.logo.dao;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class BaseDao {
+
+	@Resource
+	private SessionFactory sessionFactory;
+	
+	public Session getSession(){
+		return sessionFactory.openSession();
+	}
+}
